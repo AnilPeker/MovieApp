8261ca40cda002f51b5d9f375a1cf5e8412a95e8
Anıl Peker <anil.peker@outlook.com>
2022-08-14 19:07:03 +0300

    App configured for iOS 11

---
+++ MovieApp/AppCoordinator/AppCoordinator.swift
@@ -0,0 +1,33 @@
+//
+//  AppCoordinator.swift
+//  MovieApp
+//
+//  Created by Anıl Peker on 14.08.22.
+//
+
+import Foundation
+import UIKit
+
+// This is the basic coordinator of app
+class AppCoordinator {
+    private var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let viewController = makeMovieList()
+        self.navigationController.pushViewController(viewController, animated: false)
+    }
+}
+// MARK: - Controller Factory
+private extension AppCoordinator {
+    func makeMovieList() -> MovieListVC {
+        let viewController = MovieListVC()
+        let viewModel = MovieListVM()
+        viewController.viewModel = viewModel
+        viewModel.delegate = viewController
+        return viewController
+    }
+}
